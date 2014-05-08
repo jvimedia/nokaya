2529cbac365751b8e1ce94433aeb1b47d171c68e
Eric Dejonckheere <eric@aya.io>
2014-05-09 01:21:21 +0200

    check args

--- lib/nokaya/app.rb
+++ lib/nokaya/app.rb
@@ -10,89 +10,69 @@ module Nokaya
     map "-i" => :instagram
     option :name, aliases: "-n", type: :string, desc: "Specify a file name without extension"
     def instagram *args
-      abort Status.no_url if args.empty?
+      check_args args
       nokaya = Getter.new options, :instagram, args
       page = nokaya.parse_page
       img_link = nokaya.get_basic page
-      begin
-        download_and_save img_link, nokaya
-      rescue Interrupt
-        abort Status.canceled
-      end
+      save_pic img_link, nokaya
     end
 
     desc "favd", 'Get the photo from a Favd page (nokaya favd url)'
     option :name, aliases: "-n", type: :string, desc: "Specify a file name without extension"
     def favd *args
-      abort Status.no_url if args.empty?
+      check_args args
       nokaya = Getter.new options, :favd, args
       page = nokaya.parse_page
       img_link = nokaya.get_favd page
-      begin
-        download_and_save img_link, nokaya
-      rescue Interrupt
-        abort Status.canceled
-      end
+      save_pic img_link, nokaya
     end
 
     desc "adn", "Get the photo from a photos.app.net page (nokaya adn url)"
     option :name, aliases: "-n", type: :string, desc: "Specify a file name without extension"
     def adn *args
-      abort Status.no_url if args.empty?
+      check_args args
       nokaya = Getter.new options, :adn, args
       page = nokaya.parse_page
       img_link = nokaya.get_basic page
-      begin
-        download_and_save img_link, nokaya
-      rescue Interrupt
-        abort Status.canceled
-      end
+      save_pic img_link, nokaya
     end
 
     desc "tumblr", "Get the photo from a Tumblr photo page (nokaya -t url)"
     map "-t" => :tumblr
     option :name, aliases: "-n", type: :string, desc: "Specify a file name without extension"
     def tumblr *args
-      abort Status.no_url if args.empty?
+      check_args args
       nokaya = Getter.new options, :tumblr, args
       page = nokaya.parse_page
       img_link = nokaya.get_basic page
-      begin
-        download_and_save img_link, nokaya
-      rescue Interrupt
-        abort Status.canceled
-      end
+      save_pic img_link, nokaya
     end
 
     desc "imgur_album", "Get all images from an Imgur album (nokaya -ial url)"
     map "-ial" => :imgur_album
     def imgur_album *args
-      abort Status.no_url if args.empty?
+      check_args args
       nokaya = Getter.new options, :imgur_album, args
       page = nokaya.parse_page
       img_links = nokaya.get_imgur_album page
-      begin
-        download_album img_links, nokaya
-      rescue Interrupt
-        abort Status.canceled
-      end
+      save_album img_links, nokaya
     end
 
     desc "flickr_album", "Get all images from a Flickr album (nokaya -fal url)"
     map "-fal" => :flickr_album
     def flickr_album *args
-      abort Status.no_url if args.empty?
+      check_args args
       nokaya = Getter.new options, :flickr_album, args
       page = nokaya.parse_page
       img_links = nokaya.get_flickr_album page
-      begin
-        download_album img_links, nokaya
-      rescue Interrupt
-        abort Status.canceled
-      end
+      save_album img_links, nokaya
     end
 
     private
+
+    def check_args args
+      abort Status.no_url if args.empty?
+    end
 
     def download_and_save img_link, nokaya
       puts Status.downloading img_link
@@ -110,6 +90,22 @@ module Nokaya
         Image.save_image(file, link)
       end
       puts Status.done
+    end
+
+    def save_pic img_link, nokaya
+      begin
+        download_and_save img_link, nokaya
+      rescue Interrupt
+        abort Status.canceled
+      end
+    end
+
+    def save_album img_links, nokaya
+      begin
+        download_album img_links, nokaya
+      rescue Interrupt
+        abort Status.canceled
+      end
     end
 
   end
