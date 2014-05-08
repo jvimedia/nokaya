53627b86810ff6a297f11756f74f66874f40a3c0
Eric Dejonckheere <eric@aya.io>
2014-05-08 17:31:51 +0200

    basic rspec structure

--- lib/nokaya/app.rb
+++ lib/nokaya/app.rb
@@ -3,9 +3,9 @@ module Nokaya
   class App < Thor
     package_name "Nokaya"
 
-    desc "test", "test"
-    def test
-      puts 'ok'
+    desc "instagram", "Get the main photo from an Instagram page"
+    def instagram(*args)
+      puts args.inspect
     end
   end
 end
---
+++ spec/nokaya_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Nokaya do
+  it 'does stuff' do
+    pending # no code yet
+  end
+end
